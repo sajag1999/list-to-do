914bb4f14329351051afba1a70ce59b0026a0708
sajagchauhan <love.ambani.7@gmail.com>
2020-06-11 22:46:07 +0530

    initial commit

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Check your mails",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Get ready for the task of Today",
+        completed: true
+    },
+    {
+        id: 3,
+        text: "Check for pending works like- Application forms, LinkedIn posts, College accounts",
+        completed: true
+    },
+    {
+        id: 4,
+        text: "Phone call with a friend",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Download Money Heist Season 2 or some ",
+        completed: false
+    }
+]
+
+export default todosData
